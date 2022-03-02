c4390ecce5640b97b0b4cb7851740b82fef1d17e
Jordan <jojal@jojal.bzh>
2022-03-02 23:14:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# quarkus-osgi-reproducer
---
+++ plugins/org.sample/src/org/sample/MainApp.java
@@ -0,0 +1,35 @@
+/**
+ * Confidentiality: Restricted
+ *
+ * Copyright (C) 2022 THALES DMS FRANCE SAS.
+ * Copyright (C) 2022 THALES UK Limited.
+ * All rights reserved.
+ *
+ * The copyright in this software is the property of THALES DMS FRANCE SAS and
+ * THALES UK Limited. This software may not be used, sold, licensed, reproduced,
+ * transferred, copied, modified or reproduced in whole or in part or in any manner or
+ * form other than in accordance with the license agreement provided with this software
+ * or otherwise without the prior written consent of THALES DMS FRANCE SAS or
+ * THALES UK Limited.
+ *
+ * If we are not the intended recipient, you must not disclose, copy, circulate or in any
+ * other way use or rely on the information contained in this software. Such unauthorized
+ * use may be unlawful. If you have received this document in error, please inform:
+ * THALES UK Limited immediately on +44 (0)1932 824800 (UK), or:
+ * THALES DMS FRANCE SAS, 525 route des Dolines, 06903 Sophia Antipolis, France,
+ * Siret number: 403 215 692 00027 (FR).
+ */
+
+
+package org.sample;
+
+/**
+ * @author T0076333
+ *
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
